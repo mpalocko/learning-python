2c7785f832e827014975317b97b9b65175ceda41
Michael Palocko <palockml@mail.uc.edu>
2025-03-24 17:42:57 -0400

    Create 12_dice_roller_program.py

---
+++ mini_projects/12_dice_roller_program.py
@@ -0,0 +1,59 @@
+# Dice Roller Program
+
+import random
+
+# ● ┌ ─ ┐ │ └ ┘
+
+"┌─────────┐"
+"│         │"
+"│         │"
+"│         │"
+"└─────────┘"
+
+dice_art = {
+    1: ("┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘"),
+    2: ("┌─────────┐",
+        "│  ●      │",
+        "│         │",
+        "│      ●  │",
+        "└─────────┘"),
+    3: ("┌─────────┐",
+        "│  ●      │",
+        "│    ●    │",
+        "│      ●  │",
+        "└─────────┘"),
+    4: ("┌─────────┐",
+        "│ ●     ● │",
+        "│         │",
+        "│ ●     ● │",
+        "└─────────┘"),
+    5: ("┌─────────┐",
+        "│ ●     ● │",
+        "│    ●    │",
+        "│ ●     ● │",
+        "└─────────┘"),
+    6: ("┌─────────┐",
+        "│ ●     ● │",
+        "│ ●     ● │",
+        "│ ●     ● │",
+        "└─────────┘")
+}
+
+dice = []
+total = 0
+number_of_dice = int(input("How many dice?: "))
+
+for die in range(number_of_dice):
+    dice.append(random.randint(1,6))
+
+for die in range(number_of_dice):
+    for line in dice_art.get(dice[die]):
+        print(line)
+
+for die in dice:
+    total += die
+print(f"total: {total}")
